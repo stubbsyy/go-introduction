f0306156e02f4f4c45e99d32c16825031aa7c1ab
stubbsyy <imstubbs2@gmail.com>
2022-03-09 21:26:46 -0600

    variable basics with GO

--- video-two/lesson-two.go
+++ video-two/lesson-two.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	
+	var number int = 10
+	fmt.Println(number)
+
+	var bunchOfLetter = "this is actually a string"
+	fmt.Println(bunchOfLetter)
+
+	booleanValue := false
+	fmt.Println(booleanValue)
+
+	var hello, goodbye = 10, "not actually gone"
+	fmt.Println(hello, goodbye)
 }
