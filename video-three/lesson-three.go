2ccdd7a7fc12ed0400dfe9a1fcb0b0d026a6a9d3
stubbsyy <imstubbs2@gmail.com>
2022-04-04 19:56:47 -0500

    lesson three, not my best one

---
+++ video-three/lesson-three.go
@@ -0,0 +1,16 @@
+package main
+
+import ("fmt")
+
+func main() {
+
+	number := 5
+	//secondNumber := number % 2.0
+	//fmt.Println(secondNumber)
+
+	//fmt.Println(number / 2.0)
+	number--
+	fmt.Println(number)
+
+	fmt.Println(5 == 5)
+}
